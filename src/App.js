55ff80ba3ded3bbf8c1393c7abb15c9601b0ad14
Philip <philipmjobe@gmail.com>
2022-09-14 13:43:00 -0400

    gets names to render on the page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import React, { Component } from "react";
 import "./App.css";
+import axios from "axios";
 
-function App() {
-  return <h1>HELLO!</h1>;
+export default class Artist extends React.Component {
+  state = {
+    artists: [],
+  };
+  componentDidMount() {
+    axios.get("http://localhost:3000/artists").then((res) => {
+      const artists = res.data;
+      this.setState({ artists });
+    });
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.artists.map((artist) => (
+          <li key={artist.id}>{artist.name}</li>
+        ))}
+      </ul>
+    );
+  }
 }
 
-export default App;
+// export default Artist;

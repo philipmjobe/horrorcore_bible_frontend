e689023f0a0ca1896558e1418ecf833779f172e0
Philip <philipmjobe@gmail.com>
2022-09-16 11:20:55 -0400

    resinstalls react-bootstrap and gets some cards to show up on the screen

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import Row from "react-bootstrap/Row";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
 import "./App.css";
 import axios from "axios";
 
@@ -15,16 +18,30 @@ export default class Artist extends React.Component {
 
   render() {
     return (
-      <ul>
+      <Row xs={1} md={2} className="g-4">
         {this.state.artists.map((artist) => (
-          <>
-            <li key={artist.id}>{artist.name}</li>
-            <img src={artist.image} alt="artist" className="image" style={{ height: 400, width: 400 }} />
-          </>
+          <Col>
+            <Card>
+              <Card.Img variant="top" src="holder.js/100px160" />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This is a longer card with supporting text below as a natural lead-in to additional content. This
+                  content is a little bit longer.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
         ))}
-      </ul>
+      </Row>
     );
   }
 }
 
+{
+  /* <>
+  <li key={artist.id}>{artist.name}</li>
+  <img src={artist.image} alt="artist" className="image" style={{ height: 400, width: 400 }} />
+</>; */
+}
 // export default Artist;

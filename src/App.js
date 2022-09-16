f40f86238439647a682922784406a12844c89f9f
Philip <philipmjobe@gmail.com>
2022-09-16 12:47:20 -0400

    sorts artists alphabetically and adds nav bar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import Row from "react-bootstrap/Row";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
@@ -18,24 +22,53 @@ export default class Artist extends React.Component {
 
   render() {
     return (
-      <Row xs={1} md={3} className="g-4">
-        {this.state.artists.map((artist) => (
-          <Col>
-            <Card>
-              <Card.Img
-                variant="top"
-                src={artist.image}
-                alt="artist"
-                className="image"
-                style={{ height: 400, width: 445 }}
-              />
-              <Card.Body>
-                <Card.Title style={{ textAlign: "center" }}>{artist.name}</Card.Title>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
+      <>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+                <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <Nav>
+                <Nav.Link href="#deets">More deets</Nav.Link>
+                <Nav.Link eventKey={2} href="#memes">
+                  Dank memes
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Row xs={1} md={3} className="g-4">
+          {this.state.artists
+            .sort((a, b) => a.name.localeCompare(b.name))
+            .map((artist) => (
+              <Col>
+                <Card>
+                  <Card.Img
+                    variant="top"
+                    src={artist.image}
+                    alt="artist"
+                    className="image"
+                    style={{ height: 400, width: 445 }}
+                  />
+                  <Card.Body>
+                    <Card.Title style={{ textAlign: "center" }}>{artist.name}</Card.Title>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+        </Row>
+      </>
     );
   }
 }

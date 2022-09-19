20a7f47d440a011ddd4ce7b24e488fad02ee5ebe
Philip <philipmjobe@gmail.com>
2022-09-19 14:39:27 -0400

    removes more stuff from the navbar

--- src/App.js
+++ src/App.js
@@ -42,12 +42,12 @@ export default class Artist extends React.Component {
                     <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                   </NavDropdown> */}
                 </Nav>
-                <Nav>
+                {/* <Nav>
                   <Nav.Link href="#deets">More deets</Nav.Link>
                   <Nav.Link eventKey={2} href="#memes">
                     Dank memes
                   </Nav.Link>
-                </Nav>
+                </Nav> */}
               </Navbar.Collapse>
             </Container>
           </Navbar>

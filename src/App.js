b8a6897ebf7abe82e5aa3e22b54f048fbc423ad2
Philip <philipmjobe@gmail.com>
2022-09-19 14:38:07 -0400

    removes nav links from navbar and gives it a little flavor

--- src/App.js
+++ src/App.js
@@ -32,15 +32,15 @@ export default class Artist extends React.Component {
               <Navbar.Toggle aria-controls="responsive-navbar-nav" />
               <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="me-auto">
-                  <Nav.Link href="/show">Show</Nav.Link>
-                  <Nav.Link href="#pricing">Pricing</Nav.Link>
+                  <Nav.Link href="#">Your home for the best in the underground</Nav.Link>
+                  {/* <Nav.Link href="#pricing">Pricing</Nav.Link>
                   <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                     <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                     <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                     <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                     <NavDropdown.Divider />
                     <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                  </NavDropdown>
+                  </NavDropdown> */}
                 </Nav>
                 <Nav>
                   <Nav.Link href="#deets">More deets</Nav.Link>

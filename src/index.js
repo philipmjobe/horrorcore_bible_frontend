e6f6f0d9c9b084414ad3e9b0d3a4816943676956
Philip <philipmjobe@gmail.com>
2022-09-26 12:09:46 -0400

    moves footer, home, navigation, and show to components folder

--- src/index.js
+++ src/index.js
@@ -1,43 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Show from "./pages/Show";
-import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import { render } from "react-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Navigation, Footer, Home, Show } from "./components";
 
-// export default function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<Layout />}>
-//           <Route index element={<Home />} />
-//           <Route show element={<Show />} />
-//         </Route>
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-const rootElement = ReactDOM.createRoot(document.getElementById("root"));
-render(
-  <BrowserRouter>
+ReactDOM.render(
+  <Router>
+    <Navigation />
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route show element={<Show />} />
-      </Route>
+      <Route path="/" element={<Home />} />
+      <Route path="show" element={<Show />} />
     </Routes>
-  </BrowserRouter>,
-  rootElement
+    <Footer />
+  </Router>,
+  document.getElementById("root")
 );
-ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-export default App;

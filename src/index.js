ddec4d65921312ba3edba50b54b66549af6c774f
Philip <philipmjobe@gmail.com>
2022-10-10 13:16:25 -0400

    breaks everything trying to set up the front end working on fixing

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Navigation, Footer, Home } from "./components";
+import { Navigation, Footer, Artist } from "./components";
 
-ReactDOM.render(
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(<App tab="home" />);
+
+ReactDOM.createRoot(
   <Router>
     <Navigation />
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Artist />} />
       {/* <Route path="/show" element={<Show />} /> */}
     </Routes>
     <Footer />
   </Router>,
-  document.getElementById("root")
+  <App />,
+  document.getElementById("app")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+render(<App />, document.getElementById("root"));
